ff3a8a3d81ed4b6afc227b4c68a1d8594cc64942
Sergey <strogalev.s@gmail.com>
2022-09-07 13:15:34 +0300

    added a bit correction

--- C#_dz1/Program.cs
+++ C#_dz1/Program.cs
@@ -71,28 +71,30 @@ Task_6();
 
 //-----------------------------------------------
 /*
-Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа, в списке от 1 до N.
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
 
-/*
+
 void Task_8()
+
 {
-    int num = 1;
+    int num = 0;
     Console.Write("Введите число: ");
-    int num0 = Convert.ToInt32(Console.ReadLine());
+    int N = Convert.ToInt32(Console.ReadLine());  
 
     Console.WriteLine("Список четных чисел: ");
-    while (num < num0)
+    while (num < N)   
     {
         if (num%2 == 0) Console.Write($"{num}, ");
         num = num + 1;
     }
     Console.Write("End.");
-
 }
+
 Task_8();
-*/
+
 
 
